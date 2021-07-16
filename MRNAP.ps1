54fcb79314df9acb41e0e998709b3cdbb5de0d51
DCaz <dc>
2021-07-15 21:42:29 -0400

    Removed an S in comments

--- MRNAP.ps1
+++ MRNAP.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-  Molds Report Name And Path. Options include no date and time, no seconds with date and time, utc time.
+  Mold Report Name And Path. Options include no date and time, no seconds with date and time, utc time.
   Extension is default csv and the default directory is C:\Reports.
   Additionally -move will move out files of similar reportname to a nested directory example of default is
   C:\Reports\Old
