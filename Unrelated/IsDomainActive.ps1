6882b5f90712379e800a9de9de70cf5d951c2959
DCaz <54636357+dcazman@users.noreply.github.com>
2023-02-13 11:24:14 -0500

    version 3
    output is cleaner and more useful

--- Unrelated/IsDomainActive.ps1
+++ Unrelated/IsDomainActive.ps1
@@ -26,6 +26,7 @@ examples:
 
 Results if any comes back as an object and on host.
 #>
+#Requires -Version 5.1
 [CmdletBinding()]
 param (
     [parameter(Mandatory = $true,
@@ -43,16 +44,15 @@ param (
     [parameter(Mandatory = $false,
         HelpMessage = "Return simple true or false for A,MX,SPF,DMARC and DKIM. DKIM needs -Selector to appear.")][switch]$Boolean,
     [parameter(Mandatory = $false,
-        HelpMessage = "DKIM selector. DKIM won't be checked without this string.")][string]$Selector
+        HelpMessage = "DKIM selector. DKIM won't be checked without this string.")][string]$Selector = 'unchecked'
 )
-
+    
 <#
-ver 2,Author Dan Casmas 10/2022. Designed to work on Windows OS.
-Has only been tested with 5.1 and 7 PS Versions. Requires a minimum of PS 5.1
-Parts of this code was written by Jordan W.
-#>
-#Requires -Version 5.1
-
+    ver 3,Author Dan Casmas 02/2023. Designed to work on Windows OS.
+    Has only been tested with 5.1 and 7 PS Versions. Requires a minimum of PS 5.1
+    Parts of this code were written by Jordan W.
+    #>
+    
 #if email address pull down to domain,uri pull down to domain and if not test domain
 $TestDomain = $null
 Try {
@@ -66,7 +66,7 @@ Catch {
         [string]$TestDomain = $Domain
     }
 }
-
+    
 #Removes @
 If ([string]::IsNullOrWhiteSpace($TestDomain)) {
     Try { 
@@ -77,37 +77,31 @@ If ([string]::IsNullOrWhiteSpace($TestDomain)) {
         break script
     }
 }
-
-If ([string]::IsNullOrWhiteSpace($Selector)) {
-    $Selector = 'unchecked'
-}
-
+    
 #get the last two items in the array and join them with dot
-if (-not $Sub) {
+if (-not $Sub.IsPresent) {
     [string]$TestDomain = $TestDomain.Split(".")[-2,-1] -join "."
 }
-
+    
 #Allows a value other than true or false if dkim is selector is not provided.
 $resultdkim = 'unchecked'
-
+    
 #default for if there is an A record at all.
 [string]$resultA = If (Resolve-DnsName -Name $TestDomain -Type 'A' -Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue | Where-Object { $_.type -eq 'a' } ) { $true } Else { $false }
-
+    
 #more detail on the return
-$resultmx = $null
-If (-not $Boolean) {
-    #Brings back each record
-    $mx = Resolve-DnsName -Name $TestDomain -Type 'MX' -Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue | Sort-Object -Property Preference -ErrorAction SilentlyContinue
-    if ([string]::IsNullOrWhiteSpace($mx.NameExchange)) {
-        [string]$resultmx = $false
-    }
-    Else {
-        [string]$resultmx = "name pref ttl`n"  
-        foreach ($Item in $mx) {
-            [string]$resultmx += $Item.NameExchange + " $($Item.Preference)" + " $($Item.ttl)`n"
-        }
+If ($Boolean.IsPresent) {
+    if ($Selector -ne 'unchecked') {
+        [string]$resultdkim = If (Resolve-DnsName -Type 'TXT' -Name "$($Selector)._domainkey.$($TestDomain)" -Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue | where-object { $_.strings -match "v=DKIM1" } ) { $true } Else { $false }
     }
 
+    [string]$resultmx = If (Resolve-DnsName -Name $TestDomain -Type 'MX' -Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue | Where-Object { $_.type -eq 'mx' } ) { $true } Else { $false }
+        
+    [string]$resultspf = If (Resolve-DnsName -Name $TestDomain -Type 'TXT'-Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue | where-object { $_.strings -match "v=spf1" } ) { $true } Else { $false }
+        
+    [string]$resultDMARC = if (Resolve-DnsName -Name "_dmarc.$($TestDomain)" -Type 'TXT' -Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue | Where-Object { $_.type -eq 'txt' } ) { $true } Else { $false }
+}
+Else {
     $SPF = Resolve-DnsName -Name $TestDomain -Type 'TXT'-Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue
     $resultspf = $false
     foreach ($Item in $SPf.strings) {
@@ -116,6 +110,17 @@ If (-not $Boolean) {
         }
     }
 
+    $Mx = Resolve-DnsName -Name $TestDomain -Type 'MX' -Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue | Sort-Object -Property Preference -ErrorAction SilentlyContinue
+    if ([string]::IsNullOrWhiteSpace($Mx.NameExchange)) {
+        $resultmx = $false
+    }
+    Else {
+        $Outmx = foreach ($record in $Mx) {
+            $record | Select-object @{n = "Name"; e = { $_.NameExchange } },@{n = "Pref"; e = { $_.Preference } },TTL
+        }
+        [string]$resultmx = ($Outmx | Out-String).trimend("`r`n").Trim()
+    }
+    
     $DMARC = Resolve-DnsName -Name "_dmarc.$($TestDomain)" -Type 'TXT' -Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue
     $resultdmarc = $false
     foreach ($Item in $DMARC) {
@@ -123,7 +128,7 @@ If (-not $Boolean) {
             [string]$resultdmarc = $Item.Strings
         }
     }
-   
+       
     if ($Selector -ne 'unchecked') {
         $DKIM = Resolve-DnsName -Type 'TXT' -Name "$($Selector)._domainkey.$($TestDomain)" -Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue
         $resultdkim = $false
@@ -134,23 +139,11 @@ If (-not $Boolean) {
         }
     }
 }
-Else {
-    #brings back True or False
-    [string]$resultmx = If (Resolve-DnsName -Name $TestDomain -Type 'MX' -Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue | Where-Object { $_.type -eq 'mx' } ) { $true } Else { $false }
-
-    if ($Selector -ne 'unchecked') {
-        [string]$resultdkim = If (Resolve-DnsName -Type 'TXT' -Name "$($Selector)._domainkey.$($TestDomain)" -Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue | where-object { $_.strings -match "v=DKIM1" } ) { $true } Else { $false }
-    }
-
-    [string]$resultspf = If (Resolve-DnsName -Name $TestDomain -Type 'TXT'-Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue | where-object { $_.strings -match "v=spf1" } ) { $true } Else { $false }
-
-    [string]$resultDMARC = if (Resolve-DnsName -Name "_dmarc.$($TestDomain)" -Type 'TXT' -Server '8.8.8.8' -DnsOnly -ErrorAction SilentlyContinue | Where-Object { $_.type -eq 'txt' } ) { $true } Else { $false }
-}
-
+    
 #Output
 Return [PSCustomObject]@{
     A        = $resultA
-    MX       = $resultmx.trimend("`r`n")
+    MX       = $resultmx
     SPF      = $resultspf
     DMARC    = $resultdmarc
     DKIM     = $resultdkim
