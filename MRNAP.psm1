d86a232ad56bfa66731a494fdea4eaa729ef5f15
DCaz <54636357+dcazman@users.noreply.github.com>
2025-01-28 14:14:30 -0500

    Update MRNAP.psm1
    Ver 9 big overhaul, with new report name generator, The function will work any OS.

--- MRNAP.psm1
+++ MRNAP.psm1
@@ -1,170 +1,190 @@
 <#
 .SYNOPSIS
-Generate a file path for a report with customizable options and parameters.
+    Generates a report file name and path with various customizable options.
 
 .DESCRIPTION
-The MRNAP function creates a file path for a report based on various options and parameters.
-The function allows customization of the report name, destination directory, file extension, and various time-related options.
-It can also move similar files to an "old" directory if the Move switch is specified.
+    This script generates a report file name and path based on the provided parameters. It supports options for specifying the report name, directory, file extension, timestamp format, and more. The script can also move existing similar files to an 'old' directory if specified.
 
 .PARAMETER ReportName
-The name of the report. If not provided, a placeholder name will be used.
+    The name of the report. If not specified, a random word will be used.
 
 .PARAMETER DirectoryName
-The destination directory where the report file will be stored.
-The default value is "C:\Reports".
+    The destination directory where the report file will be stored. Default is the user's home directory.
 
 .PARAMETER Extension
-The file extension for the report file. The default value is ".csv".
-
-.PARAMETER UTC
-Use Coordinated Universal Time (UTC) for the timestamp in the file name.
-
-.PARAMETER NoSeparators
-Do not use separators (underscores and dashes) in the timestamp.
-
-.PARAMETER NoSeconds
-Exclude seconds from the timestamp.
-
-.PARAMETER JustDate
-Include only the date in the timestamp.
+    The file extension for the report file. The default value is 'csv'.
 
 .PARAMETER NoDateTimeSeconds
-Do not use the timestamp in the file name.
+    Exclude the timestamp in the file name.
+
+.PARAMETER UTC
+    Use Coordinated Universal Time (UTC) for the timestamp in the file name.
+
+.PARAMETER NoSeparators
+    Do not use separators (underscores and dashes) in the timestamp.
+
+.PARAMETER NoSeconds
+    Exclude seconds from the timestamp.
+
+.PARAMETER AddTime
+    Include time in the timestamp.
+
+.PARAMETER NoDate
+    Exclude the date in the file name.
 
 .PARAMETER Move
-Move similar files to an "old" directory if they exist.
-
-.Link
-https://github.com/dcazman/MRNAP
+    Move similar files to an 'old' directory if similar files exist.
 
 .EXAMPLE
-MRNAP -ReportName "SalesReport" -Extension ".xlsx" -JustDate -Move
-Generates a file path for a sales report with the extension ".xlsx" and includes only the date in the timestamp.
-If similar files exist, they will be moved to an "old" directory.
+    MRNAP -ReportName "SalesReport" -DirectoryName "C:\Reports" -Extension "txt" -UTC -AddTime -Move
+    Generates a report file name and path with the specified options and moves existing similar files to an 'old' directory.
 
 .EXAMPLE
-MRNAP -ReportName "MonthlyReport" -UTC -NoSeparators
-Generates a file path for a monthly report using UTC time and without separators in the timestamp.
+    MRNAP -ReportName "MonthlyReport" -UTC -NoSeparators
+    Generates a file path with name "MonthlyReport" using UTC time and without separators.
+
+.NOTES
+    Author: Dan Casmas
+    Version: 9
+    Date: 1/2025
+    Designed to work on Windows, Linux, and macOS. Tested with PowerShell 5.1 and 7.
 #>
 function MRNAP {
     [Alias("MoldReportNameAndPath")]
     #Requires -Version 5.1
     [CmdletBinding()]
     param (
-        [parameter(Position = 0, Mandatory = $False, HelpMessage = "The name of the report.")]
-        [string]$ReportName,
+        [parameter(Position = 0, Mandatory = $False, HelpMessage = "The name of the report. If not specified, a random word will be used.")]
+        [Alias("RN")][string]$ReportName,
 
-        [parameter(Position = 1, Mandatory = $False, HelpMessage = "The destination directory where the report file will be stored. The default value is 'C:\Reports'.")][string]$DirectoryName = "C:\Reports",
+        [parameter(Position = 1, Mandatory = $False, HelpMessage = "The destination directory where the report file will be stored. Default is the user's home directory.")]
+        [Alias("DN")][string]$DirectoryName,
 
-        [parameter(Position = 2, Mandatory = $False, HelpMessage = "The file extension for the report file. The default value is '.csv'.")][string]$Extension = ".csv",
+        [parameter(Position = 2, Mandatory = $False, HelpMessage = "The file extension for the report file. The default value is 'csv'.")]
+        [Alias("EXT", "E")][string]$Extension = "csv",
 
-        [parameter(Mandatory = $False, HelpMessage = "Use Coordinated Universal Time (UTC) for the timestamp in the file name.")][switch]$UTC,
+        [parameter(Mandatory = $False, HelpMessage = "Exclude the timestamp in the file name.")]
+        [Alias("NODTS")][switch]$NoDateTimeSeconds,
 
-        [parameter(Mandatory = $False, HelpMessage = "Do not use separators (underscores and dashes) in the timestamp.")][switch]$NoSeparators,
+        [parameter(Mandatory = $False, HelpMessage = "Use Coordinated Universal Time (UTC) for the timestamp in the file name.")]
+        [switch]$UTC,
 
-        [parameter(Mandatory = $False, HelpMessage = "Exclude seconds from the timestamp.")][switch]$NoSeconds,
+        [parameter(Mandatory = $False, HelpMessage = "Do not use separators (underscores and dashes).")]
+        [Alias("NoSep")][switch]$NoSeparators,
 
-        [parameter(Mandatory = $False, HelpMessage = "Include only the date in the timestamp.")][switch]$JustDate,
+        [parameter(Mandatory = $False, HelpMessage = "Exclude seconds from the timestamp.")]
+        [Alias("NoSec")][switch]$NoSeconds,
 
-        [parameter(Mandatory = $False, HelpMessage = "Exclude the timestamp in the file name. Must have -ReportName to use")][switch]$NoDateTimeSeconds,
+        [parameter(Mandatory = $False, HelpMessage = "Include time in the timestamp.")]
+        [Alias("AT")][switch]$AddTime,
 
-        [parameter(Mandatory = $False, HelpMessage = "Move similar files to an 'old' directory if they exist.")]
-        [switch]$Move
+        [parameter(Mandatory = $False, HelpMessage = "Exclude the date in the file name.")]
+        [Alias("ND")][switch]$NoDate,
+
+        [parameter(Mandatory = $False, HelpMessage = "Move similar files to an 'old' directory if similar files exist.")]
+        [Alias("M")][switch]$Move
     )
 
-    <# ver 8, Author Dan Casmas 8/2023. Designed to work on Windows OS.
-    Has only been tested with 5.1 and 7 PS Versions. Requires a minimum of PS 5.1 .#>
-
-    # ---Start of script----
-
-    # Extension Section
-    if ($Extension -ne ".csv") {
-        if (-not $Extension.StartsWith(".")) {
-            $Extension = ".$Extension"
-        }
+    # Function to get a random word
+    function GetRandomWord {
+        $Words = @(
+            'Alpha', 'Ace', 'Bravo', 'Cat', 'Dan', 'Delta', 'Dog', 'Echo', 'Ethan', 
+            'Foxtrot', 'Golf', 'Hotel', 'India', 'January', 'Juliet', 'Kathie', 'Kilo', 
+            'Lima', 'Mat', 'March', 'November', 'October', 'Oscar', 'Phil', 'Quebec', 'Romeo', 
+            'September', 'Sierra', 'Tango', 'Uniform', 'Victor', 'Whiskey', 'X-ray', 'Yoyo', 
+            'Zachary', 'Zulu'
+        )
+        return Get-Random -InputObject $Words
     }
 
-    # Build the basic ReportName with extension.
+    # Set default report name if not provided
+    if ([string]::IsNullOrWhiteSpace($ReportName)) {
+        $ReportName = GetRandomWord
+    }
+
+    # Set default directory name if not provided
+    if ([string]::IsNullOrWhiteSpace($DirectoryName)) {
+        $DirectoryName = [IO.Path]::Combine($HOME, "Reports")
+    }
+
+    # Ensure the extension starts with a dot
+    if (-not $Extension.StartsWith(".")) {
+        $Extension = ".$Extension"
+    }
+
+    # Build the basic report name with extension
     $ReportNameExt = "$ReportName$Extension"
 
-    # Handle the case when the ReportName is not provided.
-    $EmptyReportNameFlag = [string]::IsNullOrWhiteSpace($ReportName)
-
-    <# This mini function below is courtesy of the https://stackoverflow.com/ community.
-    Because Join-Path won't work with drive letters that don't exist. #>
+    # Function to join paths in a platform-independent way
     function Join-AnyPath {
-        Return ($Args -join '\') -replace '(?!^)([\\/])+', [IO.Path]::DirectorySeparatorChar
+        return ($Args -join [IO.Path]::DirectorySeparatorChar) -replace '(?!^)([\\/])+', [IO.Path]::DirectorySeparatorChar
     }
 
-    # Format the full file path based on the specified options.
-    if ($NoDateTimeSeconds) {
-        if ($EmptyReportNameFlag) {
-            Write-Warning 'ReportName needs a value to use the NoDateTimeSeconds switch.'
-            return $null
-        }
-         
-        $FullPath = Join-AnyPath $DirectoryName $ReportNameExt
-    }
-    else {
-        # General timestamp
-        $timestampFormat = "yyyy_MM_ddTHHmmss-"
+    If (-not $NoDateTimeSeconds) {
+        # Format the timestamp based on the specified options
+        $timestampFormat = "yyyy_MM_dd-"
 
-        # just date timestamp
-        if ($JustDate) {
-            $timestampFormat = "yyyy_MM_dd-"
+        if ($AddTime) {
+            $timestampFormat = "yyyy_MM_ddTHHmmss-"
         }
         elseif ($NoSeconds) {
-            # no seconds timestamp
             $timestampFormat = "yyyy_MM_ddTHHmm-"
         }
 
-        # convert to utc or standard
         if ($UTC) {
-            $timestamp = (Get-Date).ToUniversalTime().ToString($timestampFormat)
+            $timestamp = if ($NoDate) {
+            (Get-Date).ToUniversalTime().ToString("HHmmss-")
+            }
+            else {
+            (Get-Date).ToUniversalTime().ToString($timestampFormat)
+            }
         }
         else {
-            $timestamp = Get-Date -Format $timestampFormat
+            $timestamp = if ($NoDate) {
+                $null
+            }
+            else {
+                Get-Date -Format $timestampFormat
+            }
         }
 
-        # removes sepeartors
         if ($NoSeparators) {
-            $timestamp = ($timestamp -replace "_", "" -replace "-", "")
+            $timestamp = $timestamp -replace "_", "" -replace "-", ""
         }
-
-        # Removed trailing dash if no reportname
-        if ($EmptyReportNameFlag) {
-            $timestamp = ($timestamp -replace "-", "")
-        }
-
-        $FullPath = Join-AnyPath $DirectoryName "$timestamp$ReportNameExt"
     }
 
-    # Move files to "old" directory if specified.
+    # Build the full file path
+    $FullPath = Join-AnyPath $DirectoryName "$timestamp$ReportNameExt"
+
+    # Move files to "old" directory if specified
     if ($Move) {
-        if ($EmptyReportNameFlag) {
-            Write-Warning 'Move cannot move files with an empty ReportName value.'
-        }
-        else {
-            $oldDirectory = Join-AnyPath $DirectoryName "old"
-            $dirflag = $true
-            if (-not (Test-Path $oldDirectory)) {
-                Try {
-                    New-Item -ItemType Directory -Path $oldDirectory -Force -ErrorAction Stop
-                }
-                Catch {
-                    Write-Warning "Unable to create directory and move any related files to $oldDirectory"
-                    $dirflag = $False
-                }
+        if (-not (Test-Path $DirectoryName)) {
+            try {
+                New-Item -ItemType Directory -Path $DirectoryName -Force -ErrorAction Stop
             }
-            If ($dirflag) {
-                Try {
-                    Get-ChildItem -Path $DirectoryName -Filter "*$ReportNameExt" -File -ErrorAction Stop -Force
-                    Move-Item -Destination $oldDirectory -ErrorAction stop -Force
-                }
-                Catch {
-                    Write-Warning "Problem moving files to $oldDirectory."
-                }
+            catch {
+                Write-Warning "Unable to create directory $DirectoryName"
+            }
+        }
+        
+        $oldDirectory = Join-AnyPath $DirectoryName "old"
+        $dirflag = $true
+        if (-not (Test-Path $oldDirectory)) {
+            try {
+                New-Item -ItemType Directory -Path $oldDirectory -Force -ErrorAction Stop
+            }
+            catch {
+                Write-Warning "Unable to create directory and move any related files to $oldDirectory"
+                $dirflag = $false
+            }
+        }
+
+        if ($dirflag) {
+            try {
+                Get-ChildItem -Path $DirectoryName -Filter "*$ReportNameExt" -File -ErrorAction Stop -Force | Move-Item -Destination $oldDirectory -ErrorAction Stop -Force
+            }
+            catch {
+                Write-Warning "Problem moving files to $oldDirectory."
             }
         }
     }
